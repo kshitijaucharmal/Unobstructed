b1fef33130a5a7efbc9aeae571bea8007160cdc6
Kshitij Aucharmal <kshitijaucharmal21@gmail.com>
2026-09-22 23:07:23 +0530

    PS1 Shader, object specific shader

--- Scripts/dissapear.gd
+++ Scripts/dissapear.gd
@@ -4,16 +4,16 @@ extends Node3D
 @export var time_to_dissapear := 1.0
 
 var time_left := 0.0
-var mat: StandardMaterial3D
+var mat: ShaderMaterial
 
 func _ready() -> void:
 	time_left = time_to_dissapear
 	
 	if mesh_instance_3d and mesh_instance_3d.get_active_material(0):
-		mat = mesh_instance_3d.get_active_material(0).duplicate() as StandardMaterial3D
-		
-		mat.transparency = BaseMaterial3D.TRANSPARENCY_ALPHA
+		mat = mesh_instance_3d.get_active_material(0).duplicate() as ShaderMaterial
+		mat.set_shader_parameter("alpha", 1.0)
 		mesh_instance_3d.material_override = mat
+		
 
 func _process(delta: float) -> void:
 	if not mat or time_to_dissapear <= 0.0:
@@ -22,7 +22,8 @@ func _process(delta: float) -> void:
 	time_left -= delta
 	var alpha_val = clampf(time_left / time_to_dissapear, 0.0, 1.0)
 	
-	mat.albedo_color.a = alpha_val
+	# Set custom shader uniform instead of albedo_color
+	mat.set_shader_parameter("alpha", alpha_val)
 	
 	if time_left <= 0.0:
 		queue_free()
